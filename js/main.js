9d42e324b9c90e012e3728c4495efdbf641b1ad6
wky0615 <wky_0615@163.com>
2018-05-03 23:22:09 +0000

    controller

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 var app = angular.module('myApp', [])
 
-console.log($scope)
+app.controller('MyController', function($scope) {
+    $scope.person = {
+      name: 'Ari Lerner'
+    }
+})
+
+app.controller('PlayerController', ['$scope', function($scope){
+
+}])
+
+app.controller('RelatedController', ['$scope', function($scope){
+  
+}])
